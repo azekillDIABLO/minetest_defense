7a9318b02ebfcd9c2aaa04ff1a62ea3264a0f97f
Kalabasa <godffrey0@gmail.com>
2016-01-24 13:54:26 +0800

    Update minetest_game mods to 4.13

--- mods/default/aliases.lua
+++ mods/default/aliases.lua
@@ -1,6 +1,7 @@
--- aliases (Minetest 0.4 mod)
--- Provides alias for most default items
+-- mods/default/aliases.lua
 
+-- Aliases to support loading worlds using nodes following the old naming convention
+-- These can also be helpful when using chat commands, for example /giveme
 minetest.register_alias("stone", "default:stone")
 minetest.register_alias("stone_with_coal", "default:stone_with_coal")
 minetest.register_alias("stone_with_iron", "default:stone_with_iron")
@@ -65,3 +66,11 @@ minetest.register_alias("lump_of_iron", "default:iron_lump")
 minetest.register_alias("lump_of_clay", "default:clay_lump")
 minetest.register_alias("steel_ingot", "default:steel_ingot")
 minetest.register_alias("clay_brick", "default:clay_brick")
+minetest.register_alias("snow", "default:snow")
+
+-- 'mese_block' was used for a while for the block form of mese
+minetest.register_alias("default:mese_block", "default:mese")
+
+-- Aliases for corrected pine node names
+minetest.register_alias("default:pinetree", "default:pine_tree")
+minetest.register_alias("default:pinewood", "default:pine_wood")
